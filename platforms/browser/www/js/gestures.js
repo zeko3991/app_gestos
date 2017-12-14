8c81964e7dd5f3acc4f65a84c8e79d44eb187e98
zeko3991 <zeko3991@gmail.com>
2017-12-14 10:54:50 +0100

    Initial commit

---
+++ platforms/browser/www/js/gestures.js
@@ -0,0 +1,37 @@
+var app={
+	inicio: function(){
+		var botonClaro = document.querySelector('#claro');
+		var botonOscuro = document.querySelector('#oscuro');
+		
+		botonClaro.addEventListener('click',this.ponloClaro, false);
+		botonOscuro.addEventListener('click',this.ponloOscuro, false);
+	},
+	
+	iniciaHammer: function() {
+		var zona = document.getElementById('zona-gestos');
+		var hammertime = new Hammer(zona);
+		
+		hammertime.get('pinch').set({ enable: true });
+		hammertime.get('rotate').set({ enable: true });
+		
+		hammertime.on('tap doubletap swipe press pich rotate', function(ev) {
+			document.querySelector('#info').innerHTML = ev.type+'!';
+		});
+	},
+	
+	ponloClaro: function(){
+		document.body.className = 'claro';
+	},
+	
+	ponloOscuro: function(){
+		document.body.className = 'oscuro';
+	},
+	
+};
+
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		FastClick.attach(document.body);
+		app.inicio();
+	}, false);
+}
