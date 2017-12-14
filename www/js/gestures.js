c2e512aab62338c83d48cca4a12ef4e5c5ed07ac
zeko3991 <zeko3991@gmail.com>
2017-12-14 12:51:23 +0100

    swipe & rotate 1

--- www/js/gestures.js
+++ www/js/gestures.js
@@ -23,10 +23,41 @@ var app={
 		
 		hammertime.get('pinch').set({ enable: true });
 		hammertime.get('rotate').set({ enable: true });
-		
-		hammertime.on('tap doubletap swipe press pich rotate', function(ev) {
+		//pan interfiere con pinch y rotate
+		//rotate interfiere con pinch
+		/*	hammertime.on('tap doubletap pan swipe press pich rotate', function(ev) {
 			document.querySelector('#info').innerHTML = ev.type+'!';
 		});
+		*/
+		
+		zona.addEventListener('webkitAnimationEnd', function(ev) {
+			zona.className='';
+		});
+		
+		hammertime.on('doubletap', function(ev) {
+			zona.className='doubletap';
+		});
+		
+		hammertime.on('press', function(ev) {
+			zona.className='press';
+		});
+		
+		// para swipe, hammertime pasa información en el event, ( ev.direction ) 4 = derecha, 2 = izquierda
+		hammertime.on('swipe', function(ev){
+			var clase = undefined;
+			direccion = ev.direction;
+			
+			if (direccion == 4) clase = 'swipe-derecha';
+			if (direccion == 2) clase = 'swipe-izquierda';
+			
+			zona.className = clase;
+		});
+		
+		hammertime.on('rotate', function(ev) {
+			var umbral=25;
+			if (ev.distance > umbral) zona.className = 'rotate';
+		});
+		
 	},
 	
 	ponloClaro: function(){
